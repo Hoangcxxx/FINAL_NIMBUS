e3c46a72ecc0045110a41e5a80db4d4262ff89da
hieucmnep <hieuntph33888@fpt.edu.vn>
2024-10-09 10:19:45 +0700

    Moi Up

--- Back-End/src/main/java/com/example/duantn/repository/NguoiDungRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.duantn.repository;
-
-import com.example.duantn.entity.NguoiDung;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NguoiDungRepository extends JpaRepository<NguoiDung, Integer> {
-}
