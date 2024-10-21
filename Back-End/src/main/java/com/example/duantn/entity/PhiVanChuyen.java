2ecc42665a70211804021bcd785836619e8f66e1
Hoangcxxx <hoanglkph41239@fpt.edu.vn>
2024-10-21 07:27:49 +0700

    updateEntity
    chỉnh sửa lại entity sql

--- Back-End/src/main/java/com/example/duantn/entity/PhiVanChuyen.java
+++ Back-End/src/main/java/com/example/duantn/entity/PhiVanChuyen.java
@@ -17,10 +17,6 @@ public class PhiVanChuyen {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id_phi_van_chuyen")
     private Integer idPhiVanChuyen;
-
-    @Column(name = "ten_phi_van_chuyen", nullable = false)
-    private String tenPhiVanChuyen;
-
     @Column(name = "trang_thai")
     private Boolean trangThai;
 
@@ -31,4 +27,17 @@ public class PhiVanChuyen {
     @Column(name = "ngay_cap_nhat")
     @Temporal(TemporalType.TIMESTAMP)
     private Date ngayCapNhat;
+
+    @Column(name = "mo_ta")
+    private String moTa; // Thêm trường mo_ta
+
+    // Nếu cần, bạn có thể thiết lập quan hệ với DiaChiVanChuyen
+    @ManyToOne
+    @JoinColumn(name = "id_dia_chi_van_chuyen", insertable = false, updatable = false)
+    private DiaChiVanChuyen diaChiVanChuyen;
+
+    // Nếu cần, bạn có thể thiết lập quan hệ với HoaDon
+    @ManyToOne
+    @JoinColumn(name = "id_hoa_don", insertable = false, updatable = false)
+    private HoaDon hoaDon; // Giả sử bạn có một entity HoaDon
 }
